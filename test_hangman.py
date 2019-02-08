2249a61c047fda755cfed9f1af48a93a2384acb5
Noufal Ibrahim <noufal@nibrahim.net.in>
2019-02-08 16:32:11 +0530

    RED : Adds test for get_secret_word

---
+++ test_hangman.py
@@ -0,0 +1,11 @@
+# test_hangman.py
+
+import hangman
+
+# 1. Secret word should have atleast 6 letters
+# 2. Secret word should have no punctuation
+# 3. Secret word should not be a proper noun
+
+def test_secret_word_6_letters():
+    assert len(hangman.get_secret_word()) >= 6
+    
