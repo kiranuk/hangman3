3f2fc1f044f670832cf8a43f8eb2b6c2f07e0000
Noufal Ibrahim <noufal@nibrahim.net.in>
2019-02-08 16:48:32 +0530

    Adds test and implementation  for excluding proper nouns

--- hangman.py
+++ hangman.py
@@ -4,7 +4,7 @@ import random
 
 def get_secret_word(word_file="/usr/share/dict/words"):
     with open(word_file) as f:
-        words = [x.strip() for x in f if len(x.strip()) >= 6 and x.strip().isalpha()]
+        words = [x.strip() for x in f if len(x.strip()) >= 6 and x.strip().isalpha() and not x[0].isupper()]
     
     return random.choice(words)
         
