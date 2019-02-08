acfbd1c2a3e65b3d83202cd9943a666819dd3e36
Noufal Ibrahim <noufal@nibrahim.net.in>
2019-02-08 16:52:10 +0530

    Refactors hangman

--- hangman.py
+++ hangman.py
@@ -4,7 +4,15 @@ import random
 
 def get_secret_word(word_file="/usr/share/dict/words"):
     with open(word_file) as f:
-        words = [x.strip() for x in f if len(x.strip()) >= 6 and x.strip().isalpha() and not x[0].isupper()]
-    
-    return random.choice(words)
+        good_words = []
+        for i in f:
+            i = i.strip()
+            if len(i) <= 6:     # No short words
+                continue
+            if not i.isalpha(): # No punctuation
+                continue
+            if i[0].isupper():  # No proper nouns
+                continue
+            good_words.append(i)
+    return random.choice(good_words)
         
