81657f382fbee174c8d94efe656d5feb959c32df
Noufal Ibrahim <noufal@nibrahim.net.in>
2019-02-08 17:04:22 +0530

    Fixes test name

--- test_hangman.py
+++ test_hangman.py
@@ -12,7 +12,7 @@ def test_secret_word_6_letters():
 def test_secret_word_no_punctuation():
     assert all(hangman.get_secret_word("./test_data/2.words") == "fireman" for _ in range(100))
 
-def test_secret_word_no_punctuation():
+def test_secret_word_no_proper_nouns():
     assert all(hangman.get_secret_word("./test_data/3.words") == "policeman" for _ in range(100))
 
     
