4a800aaa239e368050aaf95dfc35eb845a7e4d12
kiranuk <Kiran6475@icloud.com>
2019-03-14 00:28:01 +0530

    changes made in main and guess_word

--- hangman.py
+++ hangman.py
@@ -34,26 +34,24 @@ def status(secret_word, guesses, turns):
             ' '.join(sorted(guesses)),
             turns)
 
-def guess_word(secret_word, guesses, letter, turns):
-    if letter in guesses:
-        return turns
-    else:
+def guess_word(guesses, letter, turns):
+    if letter not in guesses:
         guesses.append(letter)
         return turns - 1
+    else:
+        return turns
 
 def main():
     print("Welcome to Hangman game!")
-    success_y = success(mask, letter)
     secret_word = get_secret_word(word_file="/usr/share/dict/words")
     guesses = []
-    global turns
     turns = 10
     while True:
         mask = mask_word(secret_word, guesses)
         print(secret_word)
         print(mask)
         letter = input("Enter a letter: ")
-        turns = guess_word(secret_word, guesses, letter, turns)
+        turns = guess_word(guesses, letter, turns)
         print(status(secret_word, guesses, turns))
         if turns == 0:
             print("sorry!! The secret word was {}".format(secret_word))
