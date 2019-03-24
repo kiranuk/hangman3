b39e9d728aedfca21ae1185fba776c4cbb5c6eb7
kiranuk <Kiran6475@icloud.com>
2019-03-24 20:25:23 +0530

    Changes made by deleting some white spaces and also gives 2 lines after each functions

--- hangman.py
+++ hangman.py
@@ -2,6 +2,7 @@
 
 import random
 
+
 def get_secret_word(word_file="/usr/share/dict/words"):
     with open(word_file) as f:
         good_words = []
@@ -9,12 +10,13 @@ def get_secret_word(word_file="/usr/share/dict/words"):
             i = i.strip()
             if len(i) <= 6:     # No short words
                 continue
-            if not i.isalpha(): # No punctuation
+            if not i.isalpha():  # No punctuation
                 continue
             if i[0].isupper():  # No proper nouns
                 continue
             good_words.append(i)
     return random.choice(good_words)
+
 
 def mask_word(word, guesses):
     ret = []
@@ -25,26 +27,26 @@ def mask_word(word, guesses):
             ret.append('*')
     return ''.join(ret)
 
+
 def status(secret_word, guesses, turns):
     return """
     Secret word : {}
     Guessed letters : {}
     turns : {}
     """.format(mask_word(secret_word, guesses),
-            ' '.join(sorted(guesses)),
-            turns)
+               ' '.join(sorted(guesses)),
+               turns)
+
 
 def guess_word(secret_word, guesses, letter, turns):
-    for char in secret_word:
-        if letter == char:
-            guesses.append(char)
-            return turns
-        elif letter in guesses:
-            return turns
-        else:
-            guesses.append(letter)
-            return turns - 1
+    guesses.append(letter)
+    if letter in guesses:
         return turns
+    else:
+        guesses.append(letter)
+        return turns - 1
+
+
 def main():
     print("Welcome to Hangman game!")
     secret_word = get_secret_word(word_file="/usr/share/dict/words")
@@ -58,11 +60,12 @@ def main():
             print("\n\U0001F44d\U0001f44D You did it")
             break
         letter = input("Enter a letter: ")
-        turns = guess_word(mask, guesses, letter, turns)
+        turns = guess_word(secret_word, guesses, letter, turns)
         print(status(secret_word, guesses, turns))
         if turns == 0:
             print("sorry!! The secret word was {}".format(secret_word))
             break
+
 
 if __name__ == '__main__':
     main()
